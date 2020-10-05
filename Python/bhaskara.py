13a63e326c3eca0a4f202512e19cd2157d21b567
João Lucas Mota <joaomota59@hotmail.com>
2020-10-05 19:31:03 -0300

    Create bhaskara.py
    returns the square roots of the bhaskara formula, with the entry of the polynomial values: a, b, c

---
+++ Python/bhaskara.py
@@ -0,0 +1,15 @@
+import math
+
+a = ("a: ")
+b = ("b: ")
+c = ("c: ")
+
+delta=(b**2)-(4*a*c)
+if delta<0 or a==0:
+    print("Impossible to calculate")
+else:
+    raiz=math.sqrt(delta)
+    x1=(-b+raiz)/(2*a)
+    x2=(-b-raiz)/(2*a)
+    print("R1 = %.5f"%x1)
+    print("R2 = %.5f"%x2)
